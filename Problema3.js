a91ad96efecac673fc975b1d2c15038f070dfe88
Luis Diego <ldluisdie@gmail.com>
2024-06-09 22:05:18 -0600

    Adding more functions to the homework

---
+++ Problema3.js
@@ -0,0 +1,89 @@
+const user = { id: 1}; 
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user1);
+
+user.leerPropiedad_name = function () {
+    console.log('Leyendo la  propiedad name: ', user1.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
+
+const user = { id: 1}; 
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user2);
+
+user.leerPropiedad_name = function () {
+    console.log('Leyendo la  propiedad name: ', user2.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
+
+const user = { id: 1};  
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user3);
+
+user.leerPropiedad_name = function () { 
+    console.log('Leyendo la  propiedad name: ', user3.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
+
+const user = { id: 1}; 
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user4);
+
+user.leerPropiedad_name = function () { 
+    console.log('Leyendo la  propiedad name: ', user4.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
+
+const user = { id: 1}; 
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user5);
+
+user.leerPropiedad_name = function () { 
+    console.log('Leyendo la  propiedad name: ', user5.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
+
+const user = { id: 1}; 
+
+user.name = 'Paola';
+
+console.log('Soy el objeto user, con la nueva propiedad', user6);
+
+user.leerPropiedad_name = function () { 
+    console.log('Leyendo la  propiedad name: ', user6.name);
+}
+
+user.leerPropiedad_name();
+console.log(user);
+
+console.log('Ahora cambiaremos cosas del objeto user');
