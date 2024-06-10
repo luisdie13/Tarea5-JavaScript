f729ca73cef4d4f23460ba6adef0e7423aacab62
Luis Diego <ldluisdie@gmail.com>
2024-06-09 21:46:28 -0600

    more changes to exercises

---
+++ Problema1.js
@@ -0,0 +1,59 @@
+//usuario0
+let nombre = 'Paola';
+let apellido = 'Ortiz';
+let email = 'paola@company.ru'
+let direccion = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
+
+//usuario1
+let nombre1 = 'Paolo';
+let apellido1 = 'Ortega';
+let email1 = 'paolo@company.ru'
+let direccion1 = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
+
+//usuario2
+let nombre = 'Juan';
+let apellido = 'Ramirez';
+let email = 'juan@company.ru'
+let direccion = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
+
+//usuario3
+let nombre1 = 'Veronica';
+let apellido1 = 'Valles';
+let email1 = 'veronica@company.ru'
+let direccion1 = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
+
+//usuario4
+let nombre = 'Luis';
+let apellido = 'Acevedo';
+let email = 'luis@company.ru'
+let direccion = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
+
+//usuario5
+let nombre1 = 'Pedro';
+let apellido1 = 'Garcia';
+let email1 = 'pedro@company.ru'
+let direccion1 = {
+    municipio: 'Jocotenango',
+    calle: 'Calle ancha',
+    numero: 25,
+};
---
+++ Problema2.js
@@ -0,0 +1,89 @@
+let user1 = {                    
+    nombre: 'Paola',
+    apellido: 'Ortiz',
+    email: 'paola@company.ru',
+    direccion: {                 
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
+
+let user2 = {                    
+    nombre: 'Paolo',
+    apellido: 'Ortega',
+    email: 'paolo@company.ru',
+    direccion: {                
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
+
+let user3 = {                    
+    nombre: 'Juan',
+    apellido: 'Ramirez',
+    email: 'juan@company.ru',
+    direccion: {                 
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
+
+let user4 = {                    
+    nombre: 'Veronica',
+    apellido: 'Valles',
+    email: 'veronica@company.ru',
+    direccion: {                
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
+
+let user5 = {                    
+    nombre: 'Luis',
+    apellido: 'Acevedo',
+    email: 'luis@company.ru',
+    direccion: {                 
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
+
+let user6 = {                    
+    nombre: 'Pedro',
+    apellido: 'Garcia',
+    email: 'pedro@company.ru',
+    direccion: {                
+        municipio: 'Jocotenango',
+        calle: 'Calle ancha',
+        numero: 25,
+    },
+    estado: true,              
+    recuperarClave: function () { 
+        console.log('Recuperar clave...')
+    }
+}
